d0c96b9e4dd63d541a08784558b58be6f585ac67
parzivale <40600864+parzivale@users.noreply.github.com>
2023-03-01 09:17:04 +0800

    changed path format

--- tests/tests.rs
+++ tests/tests.rs
@@ -2,59 +2,59 @@ use std::fs;
 
 #[test]
 fn test_primitives() {
-    let code = fs::read_to_string("tests\\data\\test_primitives.pseudo").unwrap();
+    let code = fs::read_to_string("tests/data/test_primitives.pseudo").unwrap();
 
     assert!(pseudocode::interpret(code).is_ok());
 }
 
 #[test]
 fn test_primitive_type_check() {
-    let code = fs::read_to_string("tests\\data\\test_primitive_type_check.pseudo").unwrap();
+    let code = fs::read_to_string("tests/data/test_primitive_type_check.pseudo").unwrap();
 
     assert!(pseudocode::interpret(code).is_err());
 }
 
 #[test]
 fn test_array_primitives() {
-    let code = fs::read_to_string("tests\\data\\test_array_primitive.pseudo").unwrap();
+    let code = fs::read_to_string("tests/data/test_array_primitive.pseudo").unwrap();
 
     assert!(pseudocode::interpret(code).is_ok());
 }
 
 #[test]
 fn test_array_out_of_range_index() {
-    let code = fs::read_to_string("tests\\data\\test_out_of_range_index.pseudo").unwrap();
+    let code = fs::read_to_string("tests/data/test_out_of_range_index.pseudo").unwrap();
 
     assert!(pseudocode::interpret(code).is_err());
 }
 
 #[test]
 fn test_composite_primitives() {
-    let code = fs::read_to_string("tests\\data\\test_composite_primitive.pseudo").unwrap();
+    let code = fs::read_to_string("tests/data/test_composite_primitive.pseudo").unwrap();
 
     assert!(pseudocode::interpret(code).is_ok());
 }
 
 #[test]
 fn test_if_else() {
-    let code = fs::read_to_string("tests\\data\\test_if_else.pseudo").unwrap();
+    let code = fs::read_to_string("tests/data/test_if_else.pseudo").unwrap();
     assert!(pseudocode::interpret(code).is_ok());
 }
 
 #[test]
 fn test_non_key_access_composite() {
-    let code = fs::read_to_string("tests\\data\\test_non_key_access_composite.pseudo").unwrap();
+    let code = fs::read_to_string("tests/data/test_non_key_access_composite.pseudo").unwrap();
     assert!(pseudocode::interpret(code).is_err());
 }
 
 #[test]
 fn test_procedure_primitive() {
-    let code = fs::read_to_string("tests\\data\\test_procedure_primitive.pseudo").unwrap();
+    let code = fs::read_to_string("tests/data/test_procedure_primitive.pseudo").unwrap();
     assert!(pseudocode::interpret(code).is_ok());
 }
 
 #[test]
 fn test_procedure_composite() {
-    let code = fs::read_to_string("tests\\data\\test_procedure_composite.pseudo").unwrap();
+    let code = fs::read_to_string("tests/data/test_procedure_composite.pseudo").unwrap();
     assert!(pseudocode::interpret(code).is_ok());
 }
